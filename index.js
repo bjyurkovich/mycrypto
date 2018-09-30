7e7980104b282751562d5af203fb7ac33714437b
BJ Yurkovich <bjyurkovich@BJs-MacBook-Air.local>
2018-09-30 19:52:16 -0400

    swagger

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const morgan = require("morgan");
+var favicon = require("serve-favicon");
+var path = require("path");
+
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const errors = require("./middleware/errors");
 
@@ -13,9 +18,10 @@ app.set("x-powered-by", false);
 app.use(morgan("common"));
 app.use(cors());
 app.use(bodyParser.json());
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/v1", apiV1);
-
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 app.use(errors);
 
 const port = process.env.PORT || 9900;
